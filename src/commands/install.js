9bba320ec849af39dc07885e1bc80e755f34cc74
firesquid <jdeiss06@gmail.com>
2023-04-28 13:47:38 -0500

    update install

---
+++ src/commands/install.js
@@ -0,0 +1,28 @@
+module.exports = {
+  name: "install",
+  required_args: ["godot_version"],
+  optional_args: [],
+  command: (config, data, args) => {
+    // check if version is already installed
+    if (data.installed.includes(args.godot_version)) {
+      console.log(
+        `Version already installed. Type 'gdvm use ${args.godot_version}' to use it.`
+      );
+      return;
+    }
+
+    // find the link to download the version from
+    const version = data.versions.find(
+      (version) => version.version === args.godot_version
+    );
+    if (!version) {
+      console.log(
+        `Version not available. This could be because:
+         - You spelled the version wrong
+         - You haven't updated the available versions in a while with 'gdvm crawl.' Check 'gdvm available' to see what versions have been crawled.
+         - I am a bad programmer. Create an issue on GitHub if you think this is the case.`
+      );
+      return;
+    }
+  },
+};
