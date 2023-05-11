f9b8736789770c5b7886bb10f0b067135522711a
firesquid06 <jdeiss06@gmail.com>
2023-05-10 20:36:13 -0500

    resolve issue #3

--- src/utils/download.js
+++ src/utils/download.js
@@ -1,16 +1,56 @@
-const https = require("https");
+const request = require("request");
+const cliProgress = require("cli-progress");
 const fs = require("fs");
 
-// this function also takes way too long for it to be worth it to test
-const download = (url, localPath) => {
+// due to the nature of this function, it is manually tested by me. I promise that it works.
+const download = async (url, localPath) => {
+  const bar = new cliProgress.SingleBar(
+    {
+      format:
+        "Downloading [{bar}] {percentage}% | ETA: {eta}s | {value}/{total} bytes",
+    },
+    cliProgress.Presets.shades_classic
+  );
   const file = fs.createWriteStream(localPath);
-  const request = https.get(url, (response) => {
-    response.pipe(file);
+  let recievedBytes = 0;
 
-    file.on("finish", () => {
-      file.close();
-      console.log("Download complete.");
+  request
+    .get(url)
+    .on("response", (response) => {
+      if (response.statusCode !== 200) {
+        return Promise.reject(
+          `Bad status code ${response.statusCode}\n\nPlease report this issue to https://github.com/firesquid6/gdvm`
+        );
+      }
+
+      const totalBytes = response.headers["content-length"];
+      bar.start(totalBytes, 0);
+    })
+    .on("data", (chunk) => {
+      recievedBytes += chunk.length;
+      bar.update(recievedBytes);
+    })
+    .pipe(file)
+    .on("error", (err) => {
+      fs.unlink(localPath);
+      bar.stop();
+      return Promise.reject(
+        `Error downloading file with mesage: ${err.message}\n\nPlease report this issue to https://github.com/firesquid6/gdvm`
+      );
     });
+
+  file.on("finish", () => {
+    bar.stop();
+    file.close();
+    return Promise.resolve(`Downloaded ${localPath}`);
+  });
+
+  file.on("error", (err) => {
+    fs.unlink(filename);
+
+    return Promise.reject(
+      `Error writing to file with mesage: ${err.message}\n\nPlease report this issue to https://github.com/firesquid6/gdvm`
+    );
   });
 };
 
