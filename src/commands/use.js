4b87d09cbc24f4cacd64e484496c374d6237e0c8
firesquid06 <jdeiss06@gmail.com>
2023-05-19 17:56:18 -0500

    make use command extract from zip

--- src/commands/use.js
+++ src/commands/use.js
@@ -1,8 +1,57 @@
 const unzipper = require("unzipper");
+const { Store } = require("../store");
+const fs = require("fs-extra");
+const { version } = require("yargs");
 
 module.exports = {
   name: "use",
   requiredArgs: ["godotVersion"],
   optionalArgs: ["release", "mono"],
-  command: (config, args) => {},
+  command: async (config, args) => {
+    const store = new Store();
+    store.open(config.dataPath);
+
+    fs.ensureDirSync(config.godotPath);
+    let versionData = store.get("installedVersions").find((version) => {
+      return (
+        version.version === args.godotVersion &&
+        version.release === args.release &&
+        version.mono === args.mono &&
+        version.os === config.os
+      );
+    });
+
+    if (versionData === undefined) {
+      console.log("Version not found");
+      return;
+    }
+
+    const zipPath = `${config.versionsPath}/${versionData.version}-${versionData.release}-${versionData.mono}.zip`;
+
+    // unzip the file
+    fs.createReadStream(zipPath)
+      .pipe(unzipper.Extract({ path: config.godotPath }))
+      .on("finish", () => {
+        console.log("Finished unzipping");
+
+        // rename the executable
+        const executableExtensions = ["exe", "x11", "x86", "app"];
+        fs.readdirSync(config.godotPath).forEach((file) => {
+          const extension = file.split(".").pop();
+          console.log(`renaming executable to godot.${extension}`);
+          if (executableExtensions.includes(extension)) {
+            fs.renameSync(
+              `${config.godotPath}/${file}`,
+              `${config.godotPath}/godot.${extension}`
+            );
+          }
+        });
+      });
+
+    store.set("currentVersion", {
+      version: args.godotVersion,
+      release: args.release,
+      mono: args.mono,
+    });
+  },
 };
